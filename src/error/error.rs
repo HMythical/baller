c325d2eb08269919d0035021eb06dfac72752fcf
DevNitrate <ambroiseinthewild@gmail.com>
2026-02-13 01:24:14 +0100

    added some format guidelines + config file parsing

--- src/error/error.rs
+++ src/error/error.rs
@@ -4,7 +4,10 @@ use std::fmt;
 pub enum BallError {
     UnsupportedOs(String),
     UnsupportedCommand(String),
-    UnknownParameter(String)
+    UnknownParameter(String),
+    FileIoErr(std::io::Error),
+    InvalidConfig(String),
+    UnknownConfigEntry((usize, String))
 }
 
 impl fmt::Display for BallError {
@@ -14,7 +17,13 @@ impl fmt::Display for BallError {
 
             BallError::UnsupportedCommand(command) => write!(f, "the following command does not exist: {}\n\t run 'baller help' for more info", command),
 
-            BallError::UnknownParameter(param) => write!(f, "unknown parameter: '{}'\n\t run 'baller help' for more info about parameters", param)
+            BallError::UnknownParameter(param) => write!(f, "unknown parameter: '{}'\n\t run 'baller help' for more info about parameters", param),
+
+            BallError::FileIoErr(file) => write!(f, "{}", file),
+
+            BallError::InvalidConfig(msg) => write!(f, "{}", msg),
+
+            BallError::UnknownConfigEntry((line, entry)) => write!(f, "unknown config entry at line[{}]: '{}'", line, entry)
         }
     }
 }
